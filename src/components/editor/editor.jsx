4f2479207baf2ea34bc219dea1f552918b94b2f1
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-18 00:52:59 +0900

    add CardAddForm component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './editor.module.css';
+import CardEditForm from '../card_edit_form/card_edit_form';
+import CardAddForm from '../card_add_form/card_add_form';
 
-const Editor = () => {
+const Editor = ({cards, addCard}) => {
     
     return(
         <section className={styles.editor}>
-            <h1>Card Maker</h1>
+            <h1 className={styles.title}>Card Maker</h1>
+            {cards.map(card=>(
+                <CardEditForm key={card.id} card={card}/>
+            ))}
+            <CardAddForm onAdd={addCard} />
         </section>
     )
 };
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import styles from './maker.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
@@ -7,6 +7,42 @@ import Preview from '../preview/preview';
 import { useNavigate } from 'react-router-dom';
 
 const Maker = ({authService}) => {
+    const [cards,setCards]=useState([
+        {
+            id:'1',
+            name:'ellie',
+            company:'jyp',
+            theme:'light',
+            title:'engineer',
+            email:'werw@nvaer.com',
+            message:'go for it',
+            fileName:'ellei',
+            fileURL:null,
+        },
+        {
+            id:'2',
+            name:'dii',
+            company:'sm',
+            theme:'light',
+            title:'engineer',
+            email:'werw@nvaer.com',
+            message:'go for it',
+            fileName:'ellei',
+            fileURL:'ellie.png'
+        },
+        {
+            id:'3',
+            name:'btssssd',
+            company:'hybe',
+            theme:'light',
+            title:'engineer',
+            email:'werw@nvaer.com',
+            message:'go for it',
+            fileName:'ellei',
+            fileURL:null
+        },
+    ]);
+
     let navigate=useNavigate();
     const onLogout=()=>{
         authService.logout();
@@ -20,14 +56,19 @@ const Maker = ({authService}) => {
         });
     })
 
+    const addCard=(card)=>{
+        const updated=[...cards, card];
+        setCards(updated);
+    }
+
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} addCard={addCard}/>
+                <Preview cards={cards}/>
             </div>
-            <Footer />
+            {/* <Footer /> */}
         </section>
     )
 };
